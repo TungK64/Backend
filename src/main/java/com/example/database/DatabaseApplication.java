2d928b20628b11bf34f1b08c466995617b951e07
TungK64 <tungvp16@gmail.com>
2024-07-09 13:58:49 +0700

    rebuild jar file

--- src/main/java/com/example/database/DatabaseApplication.java
+++ src/main/java/com/example/database/DatabaseApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@EnableMongoRepositories(basePackages = "com.example.database.repository")
 @ComponentScan(basePackages = "com.*")
 public class DatabaseApplication {
 
